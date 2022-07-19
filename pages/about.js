7331ec62a339ae57ad1510936c6b37c161951b32
이성우 <godfgod1@iseong-uui-MacBookAir.local>
2022-07-19 09:50:26 +0900

    redirects and rewrites

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
-import Navbar from "../components/Navbar"
+import Seo from "../components/Seo"
 
 export default function About(){
 
-    
+
     return (
         <div>
-            <Navbar/>
+            <Seo title="About"/>
             <h1>hello</h1>
         </div>
     )
